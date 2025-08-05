1b0fbebbf9bdf55e0097cbff9ad71c5dc8fe6cb4
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-08-05 07:22:59 -0700

    arvr/projects/camera_samples/CameraDemoApp/app/src/main/java/com/oculus/camerademo/MainActivity.kt
    Reviewed By: zsp1987
    
    Differential Revision: D79626886
    
    fbshipit-source-id: 15f118391b640cb692adb3c2833d0a45783b3b29

--- app/src/main/java/com/oculus/camerademo/MainActivity.kt
+++ app/src/main/java/com/oculus/camerademo/MainActivity.kt
@@ -37,6 +37,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModelProvider
 import com.oculus.camerademo.ui.theme.CameraDemoTheme
@@ -160,7 +161,11 @@ class MainActivity : ComponentActivity() {
         verticalArrangement = Arrangement.Center,
     ) {
       Row(horizontalArrangement = Arrangement.SpaceBetween) {
-        Button(onClick = onStartCamera) { Text("Start camera") }
+        Button(
+            onClick = onStartCamera,
+            contentDescription = stringResource(R.string.button_description_start_camera)) {
+              Text("Start camera")
+            }
 
         Column(
             verticalArrangement = Arrangement.Center,
