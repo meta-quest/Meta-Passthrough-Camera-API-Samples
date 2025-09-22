ec65983151c8588a86e9ee5e780c76c2c0b6468a
John Nguyen <jpn@meta.com>
2025-09-21 23:21:55 -0700

    CameraDemoApp: Remove contentDescription field from Button for Gradle Build
    Summary:
    This diff removes the `contentDescription` field from `Button`.
    
    | Note: `contentDescription` was added by.AI Codemod in D79626886 to resolve the linter error `OculusJetpackComposeContentDescriptionMissing`, which did not test the build in Gradle.
    
    Builds in Android Studio use the native AndroidX Compose components, which doesn't have a `contentDescrition` field for `Button`s. See [documentation](https://developer.android.com/reference/kotlin/androidx/compose/material3/package-summary#Button(kotlin.Function0,androidx.compose.ui.Modifier,kotlin.Boolean,androidx.compose.ui.graphics.Shape,androidx.compose.material3.ButtonColors,androidx.compose.material3.ButtonElevation,androidx.compose.foundation.BorderStroke,androidx.compose.foundation.layout.PaddingValues,androidx.compose.foundation.interaction.MutableInteractionSource,kotlin.Function1)).
    
    This resulted in the error:
    ```
    e: file:///Users/jpn/Documents/CameraDemoApp/app/src/main/java/com/oculus/camerademo/MainActivity.kt:173:13 Cannot find a parameter with this name: contentDescription
    ```
    
    In removing the `contentDescription` field from `Button`, we would also need to suppress a linter warning with `SuppressWarnings("OculusJetpackComposeContentDescriptionMissing")`, however, the suppression is incompatible with Gradle builds causing the following error, so it is not included here.
    ```
    e: file:///Users/jpn/Documents/CameraDemoApp/app/src/main/java/com/oculus/camerademo/MainActivity.kt:169:17 This annotation is not applicable to target 'expression'
    ```
    
    Differential Revision: D82851633
    
    fbshipit-source-id: 2f0bde437274d161890615153894a9fed3f29c29

--- app/src/main/java/com/oculus/camerademo/MainActivity.kt
+++ app/src/main/java/com/oculus/camerademo/MainActivity.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
 package com.oculus.camerademo
 
@@ -37,7 +23,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModelProvider
 import com.oculus.camerademo.ui.theme.CameraDemoTheme
@@ -169,7 +154,6 @@ class MainActivity : ComponentActivity() {
       Row(horizontalArrangement = Arrangement.SpaceBetween) {
         Button(
             onClick = onStartCamera,
-            contentDescription = stringResource(R.string.button_description_start_camera),
         ) {
           Text("Start camera")
         }
